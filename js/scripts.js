65d7e979cc4f1a1e3edefc1866797a931797aa7f
Jase Seeley <jcseeley31590@yahoo.com>
2022-01-21 11:37:53 -0800

    WIP; result keeps coming back as 'undefined'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 function mrRoboger(num) {
   if (num === 0) {
-    return 0;
-  }
+    return "0";
+  } 
 }
-console.log(result);
+
 $(document).ready(function() {
   $("form#roboger").submit(function(event) {
     event.preventDefault();
-    var num = $("input#num").val();
-    let result = mrRoboger(num);
+    let num = $("input#num").val();
+    let result = mrRoboger(num); 
     console.log(num);
-    
-    $("#response").text(result);
-    
+    console.log(result);
+    $("#response").html(result);
   });
 });
